101804ba7bb35e9f669dee9a01f968c3165d38ed
Eduardo Santos <eduardo.debest81@gmail.com>
2020-07-02 15:30:00 -0300

    App agora esta iniciando

--- src/components/header/SearchBar.js
+++ src/components/header/SearchBar.js
@@ -4,21 +4,21 @@ import { Link } from 'react-router-dom';
 import '../../css/searchBar.css';
 
 class SearchBar extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
+    this.state = {
       searchInput: '',
     };
-	}
+  }
 
   handleSearchInput(event) {
-		const { target: { name } } = event;
+    const { target: { name } } = event;
     this.setState({ searchInput: name });
   }
 
   render() {
-		const { searchInput } = this.state;
+    const { searchInput } = this.state;
 
     return (
       <div className="searchbar">
@@ -27,7 +27,7 @@ class SearchBar extends Component {
           name="search"
           id="search"
           placeholder="Search"
-          onChange={this.handleSearchInput()}
+          onChange={this.handleSearchInput}
         />
         <div className="search-btn">
           <Link
