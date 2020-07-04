f7e14df7c05226ce306c7ddef39fd2e3c3529755
danielPantalena <pantalenadaniel@gmail.com>
2020-07-04 15:29:14 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,34 @@
 import React, { Component } from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './App.css';
 import './css/mainContents.css';
 
 import Header from './components/header/Header';
-import VideoPage from './components/content/VideoPage/VideoPage';
-import SearchResult from './components/content/SearchResult';
-import NotFound from './components/content/NotFound';
-import InitialPage from './components/content/InitialPage';
-
-const DarkTheme = React.lazy(() => import('./themes/darkTheme'));
+import Routes from './routes';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      theme: localStorage.theme || 'light',
-    }
+    this.state = { theme: localStorage.theme || 'light' };
     this.changeTheme = this.changeTheme.bind(this);
   }
 
   changeTheme() {
-    localStorage.theme = this.state.theme === 'dark' ? 'light' : 'dark';
+    const { theme } = this.state;
+    localStorage.theme = theme === 'dark' ? 'light' : 'dark';
     this.setState({ theme: localStorage.theme });
   }
 
   render() {
     const { theme } = this.state;
     return (
-      <Router>
-        <div className="App">
+      <div className={theme === 'dark' ? 'App dark' : 'App'}>
+        <BrowserRouter>
           <Header changeTheme={this.changeTheme} theme={theme} />
-          <React.Suspense fallback={<></>}>
-            {theme === 'dark' && <DarkTheme />}
-          </React.Suspense>
-          <Switch>
-            <Route exact path="/"><InitialPage /></Route>
-            <Route
-              exact
-              path="/watch/:videoId"
-              render={(props) => <VideoPage {...props} />}
-            />
-            <Route
-              exact
-              path="/results/:searchParam"
-              render={(props) => <SearchResult {...props} />}
-            />
-            <Route path="*"><NotFound /></Route>
-          </Switch>
-        </div>
-      </Router>
+          <Routes />
+        </BrowserRouter>
+      </div>
     );
   }
 }
---
+++ src/themes/DarkTheme.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './dark.css';
+
+const DarkTheme = () => <></>;
+
+export default DarkTheme;
